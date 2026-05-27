abe911226e58664b0defefa40bcd83a376aa8e4e
KARTIK CHAUDHARY <chaudharykartik7121@gmail.com>
2026-05-27 22:37:42 +0530

    data validation

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -28,7 +28,7 @@ class DataValidation:
         
     def validate_number_of_columns(self,dataframe:pd.DataFrame)->bool:
         try:
-            number_of_columns=len(self._schema_config)
+            number_of_columns=len(self._schema_config["columns"])
             logging.info(f"Required number of columns:{number_of_columns}")
             logging.info(f"Data frame has columns:{len(dataframe.columns)}")
             if len(dataframe.columns)==number_of_columns:
@@ -61,6 +61,7 @@ class DataValidation:
             dir_path = os.path.dirname(drift_report_file_path)
             os.makedirs(dir_path,exist_ok=True)
             write_yaml_file(file_path=drift_report_file_path,content=report)
+            return status
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
@@ -77,15 +78,20 @@ class DataValidation:
             
             ## validate number of columns
 
+            validation_status = True
+
             status=self.validate_number_of_columns(dataframe=train_dataframe)
+            validation_status = validation_status and status
             if not status:
                 error_message=f"Train dataframe does not contain all columns.\n"
             status = self.validate_number_of_columns(dataframe=test_dataframe)
+            validation_status = validation_status and status
             if not status:
                 error_message=f"Test dataframe does not contain all columns.\n"   
 
             ## lets check datadrift
             status=self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+            validation_status = validation_status and status
             dir_path=os.path.dirname(self.data_validation_config.valid_train_file_path)
             os.makedirs(dir_path,exist_ok=True)
 
@@ -99,9 +105,9 @@ class DataValidation:
             )
             
             data_validation_artifact = DataValidationArtifact(
-                validation_status=status,
-                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
-                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                validation_status=validation_status,
+                valid_train_file_path=self.data_validation_config.valid_train_file_path,
+                valid_test_file_path=self.data_validation_config.valid_test_file_path,
                 invalid_train_file_path=None,
                 invalid_test_file_path=None,
                 drift_report_file_path=self.data_validation_config.drift_report_file_path,
